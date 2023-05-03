d246cf7439c5c07620ca2092a879185e4fa2445f
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2023-05-03 21:30:26 +0200

    overrise webpack config with react-app-rewired

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack'); 
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
+module.exports = function override(config) { 
+		const fallback = config.resolve.fallback || {}; 
+		Object.assign(fallback, {
+      "fs": false
+      }) 
+   config.resolve.fallback = fallback; 
+   config.plugins = (config.plugins || []).concat([ 
+   	new NodePolyfillPlugin()
+   ]) 
+   return config; }
