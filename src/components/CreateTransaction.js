d799669ceb4e3aeb731d9b371e1da08409e01f61
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2023-05-06 11:06:06 +0200

    fixed create tx layout

--- src/components/CreateTransaction.js
+++ src/components/CreateTransaction.js
@@ -4,13 +4,16 @@ const CreateTransaction = (props) => {
         <p className="text-white">Now you can send a transaction to a receiver. The unsigned TX will be created and signed with the Ledger device.</p>
         
         <div className="flex flex-col items-start justify-start mt-6 space-y-4">
-        <input type="text" name="toAddress" id="toAddress" className="w-96 h-8 border-teal-300 border-b-2 bg-slate-600 px-3 py-4 focus:outline-none"
+        <input type="text" name="toAddress" id="toAddress" className="w-[50rem] h-8 border-teal-300 border-b-2 bg-slate-600 px-3 py-4 focus:outline-none"
             placeholder="To address"
         />
+        <div className="flex flex-row justify-center items-center">
         <input type="text" name="amount" id="amount" className="w-44 h-8 border-teal-300 border-b-2 bg-slate-600 px-3 py-4 focus:outline-none"
             placeholder="Amount"
         />
-        <button className="border-2 border-teal-300 rounded-md bg-slate-600 text-xs w-44 p-2 hover:bg-slate-500 active:bg-slate-500/80"
+<div className="text-white ml-2">KAS</div>
+</div>
+        <button className="border-2 border-teal-300 rounded-md bg-slate-600 text-xs p-4 hover:bg-slate-500 active:bg-slate-500/80"
         onClick={() => {props.onCreateTx && props.onCreateTx()}}
         >Create transaction</button>
         </div>
