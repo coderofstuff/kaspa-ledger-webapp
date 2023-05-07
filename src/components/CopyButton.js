2c7cf80926f3a44d59060cb84c37c2f92dc46fee
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2023-05-07 22:54:59 +0200

    added pointer cursor

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -16,6 +16,6 @@ export default (props) => {
     if (justCopied) {
         return <span><FaCheck className="mx-1 animate-spin-fast text-xl text-green-400" /></span>
     } else {
-        return <span><BiCopy className="ml-1 text-white text-xl" onClick={handleOnClick} /></span>
+        return <span><BiCopy className="ml-1 text-white text-xl hover:cursor-pointer" onClick={handleOnClick} /></span>
     }
 }
